74f161f224fbd775168bfb9522555d6cf4a0db36
Moritz Uhlig <muhlig@jsolution.de>
2011-04-27 14:22:32 +0200

    Version bump to 2.9.0.RC2

--- project/build/SBinaryProject.scala
+++ project/build/SBinaryProject.scala
@@ -1,28 +1,31 @@
 import sbt._
 
 class SBinaryProject(info: ProjectInfo) extends ParentProject(info) with NoPublish {
-	// publishing
-	override def managedStyle = ManagedStyle.Maven
-	val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  // publishing
+  override def managedStyle = ManagedStyle.Maven
 
-	lazy val core = project("core", "SBinary", new CoreProject(_))
-	lazy val treeExample = project("examples" / "bt", "Binary Tree Example", new ExampleProject(_), core)
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 
-	class ExampleProject(info: ProjectInfo) extends DefaultProject(info) with NoPublish
-	{
-		override def scratch = true
-	}
-	class CoreProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject
-	{
-	  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test"
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 
-		override def mainResources = super.mainResources +++ "LICENSE"
-	}
+  lazy val core = project("core", "SBinary", new CoreProject(_))
+
+  lazy val treeExample = project("examples" / "bt", "Binary Tree Example", new ExampleProject(_), core)
+
+  class ExampleProject(info: ProjectInfo) extends DefaultProject(info) with NoPublish  {
+    override def scratch = true
+  }
+
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) with TemplateProject {
+    def scalacheckVersion = if (buildScalaVersion == "2.8.1") "1.8" else "1.9-SNAPSHOT"
+    val scalacheck = "org.scala-tools.testing" %% "scalacheck" % scalacheckVersion % "test"
+
+    override def mainResources = super.mainResources +++ "LICENSE"
+  }
 }
-trait NoPublish extends BasicManagedProject
-{
-	override def deliverAction = publishAction
-	override def publishAction = task { None }
+
+trait NoPublish extends BasicManagedProject {
+  override def deliverAction = publishAction
+  override def publishAction = task { None }
 }
 
